d7436368f9c6cb6d700784a2a82b535a6f45fc9c
AiangGlad <114063164+AiangGlad@users.noreply.github.com>
2022-12-26 23:21:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const reveal3 = document.querySelector('.overskrift3')
 const lryic1 = document.querySelector('.lyric1')
 const lryic2 = document.querySelector('.lyric2')
 const lryic3 = document.querySelector('.lyric3')
+const video = document.getElementById('vid')
+
+video.play()
+video.style.display = 'block'
+video.setAttribute('autoplay', '');
+video.setAttribute('muted', '');
+video.setAttribute('playsinline', '');
 
 reveal.onclick = function(){
     lryic1.classList.toggle('show')
